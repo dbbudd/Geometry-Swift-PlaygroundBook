11c97c1a540f7f981e170a27875d4fd7e67d29a5
Daniel Budd <dbbudd@gmail.com>
2019-06-20 17:13:27 +0800

    Swift 5 / Xcode 11
    Initial commit to start using new Xcode 11 template for building playgrounds provided by Apple.

---
+++ PlaygroundBook/PlaygroundBook/Sources/PlaygroundAPIExtension/Pen.swift
@@ -0,0 +1,88 @@
+//
+//  Pen.swift
+//  Geometry
+//
+//  Created by Daniel Budd on 24/08/2016.
+//  Copyright © 2016 Daniel Budd. All rights reserved.
+//
+
+import SpriteKit
+
+public struct Pen {
+    public var path = UIBezierPath()
+    public var penColor = UIColor.blue
+    public var fillColor = UIColor.clear
+    public var lineWidth: CGFloat = 3.0
+    public var currentHeading: Double = 0.0
+    
+    public var position = CGPoint(x:0, y:0) {
+        didSet {
+            self.path.move(to: self.position)
+        }
+    }
+    
+    public init(){
+        defer {
+            self.position = CGPoint(x: 0, y: 0)
+        }
+    }
+
+    public func addLine(distance: Double){
+        
+        let headingInRadians = self.currentHeading * (Double.pi / 180) //convert to radians
+        let dx = distance * cos(headingInRadians)
+        let dy = distance * sin(headingInRadians)
+        let currentX = Double(self.path.currentPoint.x)
+        let currentY = Double(self.path.currentPoint.y)
+        
+        self.path.addLine(to: CGPoint(x: currentX + dx, y: currentY + dy))
+        self.path.stroke()
+
+    }
+    
+    public func move(distance: Double){
+        let headingInRadians = self.currentHeading * (Double.pi / 180) //convert to radians
+        let dx = distance * cos(headingInRadians)
+        let dy = distance * sin(headingInRadians)
+        let currentX = Double(self.path.currentPoint.x)
+        let currentY = Double(self.path.currentPoint.y)
+        
+        self.path.move(to: CGPoint(x: currentX + dx, y: currentY + dy))
+        self.path.stroke()
+    }
+    
+    
+    
+    public func goto(dx: Double, dy: Double){
+        let currentX = Double(self.path.currentPoint.x)
+        let currentY = Double(self.path.currentPoint.y)
+        
+        self.path.move(to: CGPoint(x: currentX + dx, y: currentY + dy))
+        self.path.stroke()
+    }
+    
+    public func drawTo(dx: Double, dy: Double){
+        let currentX = Double(self.path.currentPoint.x)
+        let currentY = Double(self.path.currentPoint.y)
+        
+        self.path.addLine(to: CGPoint(x: currentX + dx, y: currentY + dy))
+        self.path.stroke()
+    }
+    
+    public mutating func turn(degrees: Double){
+        self.currentHeading = self.currentHeading + degrees
+    }
+    
+    public func currentPosition() -> CGPoint{
+        let position = self.path.currentPoint
+        
+        return position
+    }
+    
+    public func drawCircle(radius: CGFloat){
+        self.path.addArc(withCenter: self.path.currentPoint, radius: radius, startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
+    }
+    
+}
+
+
